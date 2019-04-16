12979507319cf8806ff48ad583e4730013286f7a
Nick Zadvorniy <kolyazdv@gmail.com>
2019-04-16 17:18:39 +0300

    Add Users to Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  as :user do
+    get "signin", to: 'devise/sessions#new'
+    delete "signout", to: 'devise/sessions#destroy'
+    get 'signup', to: 'devise/registrations#new'
+  end
+
   root 'pages#home'
   get 'about' => 'pages#about'
 
