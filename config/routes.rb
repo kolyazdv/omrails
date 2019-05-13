0c90bd45da517166918aeac59d38710dc95f4b2f
Nick Zadvorniy <kolyazdv@gmail.com>
2019-05-13 18:30:06 +0300

    Add user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/:username', to: 'users#show', as: 'user'
   resources :items
   resources :tweets
   ActiveAdmin.routes(self)
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
   end
 
   root 'pages#home'
-  get 'about' => 'pages#about'
+  get 'about', to: 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
